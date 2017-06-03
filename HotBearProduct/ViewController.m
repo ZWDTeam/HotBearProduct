d9fea9df775f3135611aba2510ddd2e1ffce10f2
zhongweidi <506270233@qq.com>
2017-06-04 01:21:19 +0800

    测试

--- HotBearProduct/ViewController.m
+++ HotBearProduct/ViewController.m
@@ -16,13 +16,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    //主线路
+
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
